edae7b52d668825fa4d3cf11d0b2269e11cb3f73
zarvis.a@gmail.com <zarvis.a@gmail.com@3330bb89-21d6-6003-22c0-83b921a2f836>
2013-09-20 08:18:40 +0000

    Updated the Code and Removed unnecessary comments

--- trunk/com.jetcore.site/src/java/com/app/jetcore/test/service/ServiceImplTest.java
+++ trunk/com.jetcore.site/src/java/com/app/jetcore/test/service/ServiceImplTest.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.app.jetcore.test.service;
 
 import com.app.jetcore.test.domain.JetCoreTestCase;
