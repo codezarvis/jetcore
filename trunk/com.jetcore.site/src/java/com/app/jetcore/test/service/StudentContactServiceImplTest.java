7e534d64f841de54103d8fa9274ee222c634e63e
zarvis.a@gmail.com <zarvis.a@gmail.com@3330bb89-21d6-6003-22c0-83b921a2f836>
2013-09-23 04:51:22 +0000

    Added StudentContactServiceImplTest to the App

---
+++ trunk/com.jetcore.site/src/java/com/app/jetcore/test/service/StudentContactServiceImplTest.java
@@ -0,0 +1,51 @@
+package com.app.jetcore.test.service;
+
+import com.app.jetcore.context.id.names.ContextIdNames;
+import com.app.jetcore.main.domain.service.StudentContactService;
+import com.app.jetcore.main.domain.sub.StudentContact;
+import com.app.jetcore.main.utils.AppContext;
+import com.app.jetcore.test.domain.JetCoreTestCase;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sudarsan
+ */
+public class StudentContactServiceImplTest extends JetCoreTestCase {
+    
+    private static Logger LOG = Logger.getLogger(StudentContactServiceImplTest.class);
+    
+    @Test
+    public void testFindByEnrollmentNumber() {
+        
+        StudentContactService studentContactService = (StudentContactService) AppContext.APPCONTEXT.getBean(ContextIdNames.STUDENT_CONTACT_SERVICE);
+        StudentContact studentContact = studentContactService.findByEnrollmentNumber("111");
+
+        //LOG.debug(studentContact.toString());
+        assertNotNull(studentContact);
+        
+    }
+    
+    @Test
+    public void testFindByEmail() {
+        StudentContactService studentContactService = (StudentContactService) AppContext.APPCONTEXT.getBean(ContextIdNames.STUDENT_CONTACT_SERVICE);
+        StudentContact studentContact = studentContactService.findByEmail("student1@cmc.com");
+
+        //LOG.debug(studentContact);
+        assertNotNull(studentContact);
+        
+        
+    }
+    
+    @Test
+    public void testFindByMobile() {
+        StudentContactService studentContactService = (StudentContactService) AppContext.APPCONTEXT.getBean(ContextIdNames.STUDENT_CONTACT_SERVICE);
+        StudentContact studentContact = studentContactService.findByMobile("9676534400");
+        
+        LOG.debug(studentContact);
+        
+        assertNotNull(studentContact);
+    }
+}
