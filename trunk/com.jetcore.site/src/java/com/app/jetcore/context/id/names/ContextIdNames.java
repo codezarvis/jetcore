41890ca3f4db957ebf49d35835c89f3adfd5e3bc
zarvis.a@gmail.com <zarvis.a@gmail.com@3330bb89-21d6-6003-22c0-83b921a2f836>
2013-10-23 09:18:51 +0000

    Updated ContextIdNames

--- trunk/com.jetcore.site/src/java/com/app/jetcore/context/id/names/ContextIdNames.java
+++ trunk/com.jetcore.site/src/java/com/app/jetcore/context/id/names/ContextIdNames.java
@@ -12,9 +12,15 @@ public interface ContextIdNames {
     
     public static final String STUDENT_CONTACT = "studentContact";
     
+    public static final String STUDENT_ACEDAMIC_DETAIL = "studentAcedamicDetail";
+    
+    public static final String DATE_FORMAT = "dateFormat";
+    
     public static final String JET_USER_SERVICE = "jetUserService";
     
     public static final String STUDENT_REGISTRATION_SERVICE = "studentRegistrationService";
     
     public static final String STUDENT_CONTACT_SERVICE = "studentContactService";
+    
+    public static final String STUDENT_ACEDAMIC_DETAIL_SERVICE = "studentAcedamicDetailService";
 }
