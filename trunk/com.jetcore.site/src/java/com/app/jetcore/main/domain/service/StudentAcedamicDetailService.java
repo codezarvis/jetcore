2206f18d1aca2cbfb52dce53489ca7e47413e9ab
zarvis.a@gmail.com <zarvis.a@gmail.com@3330bb89-21d6-6003-22c0-83b921a2f836>
2013-11-08 03:55:49 +0000

    Changed StudentAcedamicDetailService.java

--- trunk/com.jetcore.site/src/java/com/app/jetcore/main/domain/service/StudentAcedamicDetailService.java
+++ trunk/com.jetcore.site/src/java/com/app/jetcore/main/domain/service/StudentAcedamicDetailService.java
@@ -2,6 +2,7 @@
 package com.app.jetcore.main.domain.service;
 
 import com.app.jetcore.main.domain.sub.StudentAcedamicDetail;
+import java.util.List;
 
 /**
  *
@@ -10,5 +11,7 @@ import com.app.jetcore.main.domain.sub.StudentAcedamicDetail;
 public interface StudentAcedamicDetailService extends Service{
     
     StudentAcedamicDetail findByEnrollmentNumber(String enrollmentNumber);
+    
+    List<StudentAcedamicDetail> findByQualfAndMarks(String qualf, String marks);
     
 }
