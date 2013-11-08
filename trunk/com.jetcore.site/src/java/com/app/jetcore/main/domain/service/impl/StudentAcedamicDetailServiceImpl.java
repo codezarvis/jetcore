2e4ce512edc04487b25e1bd68bf992b29506d370
zarvis.a@gmail.com <zarvis.a@gmail.com@3330bb89-21d6-6003-22c0-83b921a2f836>
2013-11-08 03:56:13 +0000

    Changed StudentAcedamicDetailServiceImpl

--- trunk/com.jetcore.site/src/java/com/app/jetcore/main/domain/service/impl/StudentAcedamicDetailServiceImpl.java
+++ trunk/com.jetcore.site/src/java/com/app/jetcore/main/domain/service/impl/StudentAcedamicDetailServiceImpl.java
@@ -6,7 +6,13 @@ import com.app.jetcore.main.domain.service.StudentRegistrationService;
 import com.app.jetcore.main.domain.sub.StudentAcedamicDetail;
 import com.app.jetcore.main.domain.sub.StudentRegistration;
 import com.app.jetcore.main.utils.AppContext;
+import com.app.jetcore.main.utils.QualfMap;
+import com.app.jetcore.main.utils.hibernate.HibernateUtils;
+import java.util.List;
 import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 /**
  *
@@ -25,12 +31,30 @@ public class StudentAcedamicDetailServiceImpl extends ServiceImpl implements Stu
 
         try {
             StudentRegistrationService studentRegistrationService = (StudentRegistrationService) AppContext.APPCONTEXT.getBean(ContextIdNames.STUDENT_REGISTRATION_SERVICE);
-            StudentRegistration studentRegistration= studentRegistrationService.findByEnrollmentNumber(enrollmentNumber);
+            StudentRegistration studentRegistration = studentRegistrationService.findByEnrollmentNumber(enrollmentNumber);
             studentAcedamicDetail = studentRegistration.getStudentAcedamicDetail();
         } catch (Exception exception) {
             LOG.debug("StudentAcedamicDeatilServiceImpl", exception);
         }
 
         return studentAcedamicDetail;
+    }
+
+    @Override
+    public List<StudentAcedamicDetail> findByQualfAndMarks(String qualf, String marks) {
+        List<StudentAcedamicDetail> list = null;
+        Session session = HibernateUtils.currentSession();
+        String qualification = QualfMap.getQualf(qualf);
+        LOG.debug("==================Qualf is : "+qualification);
+        try {
+            Criteria criteria = session.createCriteria(StudentAcedamicDetail.class);
+            criteria.add(Restrictions.eq(qualification, qualf)).add(Restrictions.ge(qualification, marks));
+            list = criteria.list();
+        } catch (Exception exception) {
+            LOG.warn("StudentAcedamicDeatilServiceImpl", exception);
+        } finally {
+            HibernateUtils.closeSession();
+        }
+        return list;
     }
 }
